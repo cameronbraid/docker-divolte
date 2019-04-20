e3b8af2763b88e38fb2a28154b8ceb0317078469
Cameron Braid <cameron@drivenow.com.au>
2019-04-20 21:49:09 +1000

    added watch events scripts

--- watch-events.sh
+++ watch-events.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 oc rsh kafka-schema-registry-0 kafka-avro-console-consumer \
     --bootstrap-server kafka-kafka-bootstrap:9092 \
-    --topic divolte
+    --topic divolte | jq --unbuffered -R -r '. as $line | try fromjson catch $line'
